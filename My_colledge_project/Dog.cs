236e488d14adb51af45c35d3e6dcb97918769c6e
Andrey Pershin <anchizh93@gmail.com>
2019-11-09 09:16:33 +0400

    Create Dog.cs

---
+++ My_colledge_project/Dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Dog
+{
+    private int height;
+    private int score;
+    private int health;
+    private int speed;
+    public Dog(int height)
+	{
+        this.height = height;
+        this.health = 100;
+        this.score = 0;
+        this.speed = 0;
+	}
+    public void move()
+    {
+        this.speed = 10;
+    }
+
+
+
+
+}
