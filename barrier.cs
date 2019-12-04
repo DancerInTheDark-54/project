86ee08873114bf0a60e0bf9893ff7043c5e4b2cd
DancerInTheDark-54 <57007423+DancerInTheDark-54@users.noreply.github.com>
2019-12-04 09:10:56 +0400

    Добавил новые классы (Некоторые WIP)

---
+++ barrier.cs
@@ -0,0 +1,19 @@
+  
+using System;
+
+public class Barrier
+{
+    private int height;
+    private int width;
+    private bool interact;
+    public  void Door(bool interact)
+	{
+	}
+    public void CarCrash()
+    {
+    }
+
+
+
+
+}
---
+++ bonus.cs
@@ -0,0 +1,26 @@
+  
+using System;
+
+public class Bonus
+{
+    private int b_hunger;
+    private int b_score;
+    private int b_health;
+    private int b_speed;
+    public void OnHunger()
+	{
+	}
+    public void OnHealth()
+    {
+    }
+    public void OnScore()
+    {
+    }
+    public void OnSpeed()
+    {
+    }
+
+
+
+
+}
