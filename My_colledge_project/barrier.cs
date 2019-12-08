4a24ea37fb5c50373968ae0ba498049cf1d582d6
DancerInTheDark-54 <57007423+DancerInTheDark-54@users.noreply.github.com>
2019-12-08 13:51:40 +0400

    Добавил конструкторы. Зарисовок еще нет

---
+++ My_colledge_project/barrier.cs
@@ -0,0 +1,30 @@
+  
+using System;
+
+public class Barrier
+{
+    private int height;
+    private int width;
+    private bool interact;
+    public Door
+      { this.height = height;
+        this.width = width;
+        this.interact = false;
+      }
+   public Car 
+    { this.height = 0;
+      this.width = 0;
+      this.speed = this.height*this.width*0.5;
+    }
+    public  void Door(bool interact)
+	{
+	}
+    public void CarCrash()
+    {
+    }
+
+
+
+
+}
+
---
+++ My_colledge_project/bonus.cs
@@ -0,0 +1,41 @@
+  
+using System;
+
+public class Bonus
+{
+    private int b_hunger;
+    private int b_score;
+    private int b_health;
+    private int b_speed;
+    public OnHunger
+    { this.b_hunger = b_hunger;
+     }
+    public OnHealth
+    {
+     this.b_health = b_health;
+    }
+    public OnScore
+    {
+     this.b_score = b_score;
+    }
+    public OnSpeed
+    {
+     this.b_speed = b_speed;
+    }
+    public void OnHunger()
+	{
+	}
+    public void OnHealth()
+    {
+    }
+    public void OnScore()
+    {
+    }
+    public void OnSpeed()
+    {
+    }
+
+
+
+
+}
